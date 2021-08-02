f46491e6d2c3a5c2aef9af9e32fc24ae5fc70725
Elliott7 <elliottbreadmore@hotmail.com>
2021-08-02 15:50:50 +1000

    Creation of Mail Merge project - creates letters for all of different invitees.

--- Intermediate Project 24 - Mail Merge - 2-8.py
+++ Intermediate Project 24 - Mail Merge - 2-8.py
@@ -1,8 +1,14 @@
 """
 Project Twenty Four - Mail Merge
+Project to automate the creation of files and text invites. Easy but really cool
 """
 
-file = open("C:/Users/User/Documents/snakescore.txt")
-contents = file.read()
-print(contents)
-file.close()
+invitation = "You are invited to my birthday this Saturday \n \nHope you can make it! \n \nMe"
+with open("names.txt") as file:
+    contents = file.read().split()
+    print(contents)
+    for name in contents:
+        string = f"Dear {name}, \n \n" + invitation
+        with open(f"letter_for_{name}.txt", 'w') as filee:
+            filee.write(string)
+
